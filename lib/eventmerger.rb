b2822b4766c5dcea8e5d6ac8e362a30eb76a60a7
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-10-08 14:50:38 -0400

    Normalize dates, deal with duplicate keys

--- lib/eventmerger.rb
+++ lib/eventmerger.rb
@@ -1,39 +1,55 @@
+require 'date'
+
 class EventMerger
-  def self.merge!(events)
+  attr_reader :events 
+
+  def initialize(events)
     raise ArgumentError.new("Events need to be provided within an Array") unless events.is_a? Array
     @events = events
+  end 
+
+  def merge!    
     group_events(uniq_dates) 
   end
 
   private 
 
-  class << self 
-    def uniq_dates
-      begin
-        @events.map { |event| event[:date] }.uniq
-      rescue => e
-        raise ArgumentError.new("Events need to be in the form of a Hash")
-      end 
-    end
-
-    def group_events(dates)
-      return "No dates provided" if dates.compact.empty? 
-      event_array = []
-      dates.each do |date|
-        grouped_events = @events.select { |event| event[:date] == date }
-        event_array << merge_keys(grouped_events, date)
-      end
-      event_array 
+  def uniq_dates
+    begin
+      events.map { |event| normalize_date(event[:date]) }.uniq
+    rescue 
+      raise ArgumentError.new("Events need to be in the form of a Hash")
     end 
+  end
 
-    def merge_keys(events, date)
-      events.inject({date: date}) do |merged_hash, event|
-        event.each do |key, value|
-          return if key == date
+  def normalize_date(date)
+    begin
+      Date.parse(date).strftime("%Y-%m-%d")
+    rescue
+      raise ArgumentError.new("Event did not include date")
+    end 
+  end 
+
+  def group_events(dates)
+    event_array = []
+    dates.each do |date|
+      grouped_events = events.select { |event| event[:date] == date }
+      event_array << merge_keys(grouped_events, date)
+    end
+    event_array 
+  end 
+
+  def merge_keys(events, date)
+    events.inject({date: date}) do |merged_hash, event|
+      event.each do |key, value|
+        next if key == :date
+        if merged_hash.key?(key)
+          merged_hash[key] = ([merged_hash[key]] <<  value).flatten
+        else 
           merged_hash[key] = value
         end 
-        merged_hash
       end 
-    end
-  end 
+      merged_hash
+    end 
+  end
 end
--- spec/eventmerger_spec.rb
+++ spec/eventmerger_spec.rb
@@ -3,12 +3,15 @@ require 'spec_helper'
 describe EventMerger do
   describe "#merge!" do 
     before(:example) do 
-      @example_1 = EventMerger.merge!([{date: '2014-01-01', a: 5, b:1}, {date: '2014-01-01', xyz: 11},
-                                      {date: '2014-10-10', qbz: 5}, {date: '2014-10-10', v: 4, q: 1, strpm: -99}])
-      @example_2 = EventMerger.merge!([{date: '2016-01-01', a: 1, b: 2}, {date: '2016-01-01', c: 3, d: 4, e: 5}, 
-                                      {date: '2016-01-01', f: 17, g: 15391}, {date: '2016-01-01', h: 'foo', i: 'bar', j: 'baz'},
-                                      {date: '2016-01-02', a: 1, b: 2}, {date: '2016-01-02', c: 64, d: 1324, e: -123, f: 56},
-                                      {date: '2016-01-03', m: 37, n: 236509, o: -1351352, g: 'foobar'}])
+      @example_1 = EventMerger.new([{date: '2014-01-01', a: 5, b:1}, {date: '2014-01-01', xyz: 11},
+                                    {date: '2014-10-10', qbz: 5}, {date: '2014-10-10', v: 4, q: 1, strpm: -99}]).merge!
+      @example_2 = EventMerger.new([{date: '2016-01-01', a: 1, b: 2}, {date: '2016-01-01', c: 3, d: 4, e: 5}, 
+                                    {date: '2016-01-01', f: 17, g: 15391}, {date: '2016-01-01', h: 'foo', i: 'bar', j: 'baz'},
+                                    {date: '2016-01-02', a: 1, b: 2}, {date: '2016-01-02', c: 64, d: 1324, e: -123, f: 56},
+                                    {date: '2016-01-03', m: 37, n: 236509, o: -1351352, g: 'foobar'}]).merge!
+      @example_3 = EventMerger.new([{date: '01-01-2016', a: 2, b: 3}, {date: '2016-01-01', c: 4, d: 5}]).merge!
+      @example_4 = EventMerger.new([{date: '2016-01-01', a: 1, b: 2}, {date: '2016-01-01', a: 3, b: 4}, 
+                                    {date: '2016-01-01', a: 5, b: 6}]).merge!
     end 
 
     it "should return an array of hashes" do 
@@ -43,17 +46,24 @@ describe EventMerger do
       expect(@example_2[2].keys).to eq (%i(date m n o g))
     end
 
+    it "should return the dates in the same format" do 
+      expect(@example_3[0][:date]).to eq("2016-01-01")
+    end
+
+    it "should create an array of values if the key already exists" do 
+      expect(@example_4[0]).to eq({date: '2016-01-01', a: [1, 3, 5], b: [2, 4, 6]})
+    end 
+
     it "should raise an ArgumentError if arguments aren't in an Array" do 
-      expect{ EventMerger.merge!("False") }.to raise_error(ArgumentError)
+      expect{ EventMerger.new("False") }.to raise_error(ArgumentError)
     end
 
     it "should raise an ArgumentError if the Events aren't in a hash" do 
-      expect { EventMerger.merge!(["false"])}.to raise_error(ArgumentError)
+      expect { EventMerger.new(["false"]).merge! }.to raise_error(ArgumentError)
     end
 
-    it "should return a message if no dates provided" do
-      @output = EventMerger.merge!([{d: 1}, {m: 2}])
-      expect(@output).to eq("No dates provided")
+    it "should raise an ArgumentError if no dates provided" do
+      expect { EventMerger.new([{d: 1}, {m: 2}]).merge! }.to raise_error(ArgumentError)
     end 
   end 
 end
